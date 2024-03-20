f1fceb3c1171958849ea67b1e2b88c764545c7eb
CoyleWasHere <dancoyle321@gmail.com>
2024-03-20 19:13:51 -0400

    Initial Commit

---
+++ Hues and Clues/Hues_and_CluesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hues_and_CluesApp.swift
+//  Hues and Clues
+//
+//  Created by Daniel Coyle on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Hues_and_CluesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
